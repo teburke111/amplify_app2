96fa9e5f5734161b48b0ae9db66114e2e464957d
teburke111 <teburke111@gmail.com>
2025-09-09 22:41:17 -0400

    updated

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Amplify} from 'aws-amplify';
-import config from './aws-exports';
+import awsconfig from './aws-exports';
 
-Amplify.configure(config);
+Amplify.configure(awsconfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
